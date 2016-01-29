e39a6445e59406999d8813915648a02fe5470416
tech1arb <tech1arb@missionrnd.com>
2016-01-29 20:44:32 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -18,7 +18,33 @@ struct node {
 	int num;
 	struct node *next;
 };
+int linkedListMedian(struct node *head)
+{
+	if (head == NULL)
+		return -1;
+	struct node *slow_ptr = head;
+	struct node *fast_ptr = head;
+	fast_ptr = fast_ptr->next;
+	int result=0;
+	if (head != NULL)
+	{
+		while (fast_ptr != NULL && fast_ptr->next != NULL)
+		{
+			fast_ptr = fast_ptr->next->next;
+			slow_ptr = slow_ptr->next;
+		}
 
-int linkedListMedian(struct node *head) {
-	return -1;
+		//printf("The middle element is [%d]\n\n", slow_ptr->data);
+	}
+	if (fast_ptr == NULL)
+	{
+		result = slow_ptr->num;
+	}
+	else
+	{
+		result = (((slow_ptr->num + (slow_ptr->next->num))) / 2);
+	}
+
+	
+	return result;
 }
