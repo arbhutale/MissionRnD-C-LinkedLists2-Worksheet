5482322bef3ebf5038670cdf32ec7342652f3603
tech1arb <tech1arb@missionrnd.com>
2016-01-30 05:19:18 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,47 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<assert.h>
 struct node {
 	int num;
 	struct node *next;
 };
+void MoveNode(struct node** destRef, struct node** sourceRef)
+{
+	struct node* newNode = *sourceRef;
+	assert(newNode != NULL);
+	*sourceRef = newNode->next;
+	newNode->next = *destRef;
+	*destRef = newNode;
+}
+struct node * merge2LinkedLists(struct node *head1, struct node *head2)
+{
+	struct node* result = NULL;
 
-struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	struct node** lastPtrRef = &result;
+
+	while (1)
+	{
+		if (head1 == NULL)
+		{
+			*lastPtrRef = head2;
+			break;
+		}
+		else if (head2 == NULL)
+		{
+			*lastPtrRef = head1;
+			break;
+		}
+		if (head1->num <= head2->num)
+		{
+			MoveNode(lastPtrRef, &head1);
+		}
+		else
+		{
+			MoveNode(lastPtrRef, &head2);
+		}
+
+		lastPtrRef = &((*lastPtrRef)->next);
+	}
+	return(result);
 }
